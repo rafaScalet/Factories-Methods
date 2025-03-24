108c5494057c28b57682953862e80aba8820af46
Rafael Scalet <114953004+rafaScalet@users.noreply.github.com>
2025-03-24 16:08:47 -0300

    add gamma link

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Factories Methods
+
+[Link do Gamma](https://gamma.app/docs/Design-Patterns-Criacionais-Factory-Method-e-Abstract-Factory-6f544nfo3jcjf32)
--- src/factoryMethod/Main.java
+++ src/factoryMethod/Main.java
@@ -8,12 +8,12 @@ import factoryMethod.factory.PostgresFactory;
 public class Main {
   public static void main(String[] args) {
     ConnectionFactory factoryPostgres = new PostgresFactory();
-    Connection connectionPostgres = factoryPostgres.createConnection("Postgres");
+    Connection connectionPostgres = factoryPostgres.createConnection("postgres://root:1234@localhost:2345/my_db");
 
     connectionPostgres.connect();
 
     ConnectionFactory factoryMysql = new MySQLFactory();
-    Connection connectionMysql = factoryMysql.createConnection("MySQL");
+    Connection connectionMysql = factoryMysql.createConnection("mysql://root:1234@localhost:3306/my_db");
 
     connectionMysql.connect();
   }
